0721208d62909007f72a79f07a08520911903974
donggyu-kim <kimss7334@naver.com>
2021-05-30 23:28:34 +0900

    21.05.30

--- BOJ/16234.py
+++ BOJ/16234.py
@@ -1,83 +1,58 @@
 from collections import deque
 from copy import deepcopy
 
-N,L,R = map(int,input().split())
+N, L, R = map(int,input().split())
 
 field = []
-visited = [[False for _ in range(N)] for _ in range(N)]
-allow = [[0 for _ in range(N)] for _ in range(N)]
 
 for _ in range(N):
     field.append(list(map(int,input().split())))
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-# c_num = 1
 
-def check(field, x, y):
+def bfs(x, y):
     dq = deque()
-    flag = False
+    union = []
     dq.append((x,y))
-    cnum = 1
+    union.append((x,y))
+    visited[x][y] = True
     while dq:
-        x,y = dq.popleft()
-        visited[x][y] = True
+        x, y = dq.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-
-            if 0 <= nx < N and 0 <= ny < N:
-                if L <= abs(field[x][y] - field[nx][ny]) <= R:
-                    allow[x][y] = cnum
-                    allow[nx][ny] = cnum
-                if not visited[nx][ny]:
-                    dq.append((nx,ny))
-    return flag
-
-def move(arr):
-    union_idx = []
-    total = 0
-    cnt = 0
-    for i in range(N):
-        for j in range(N):
-            if allow[i][j] == True:
-                union_idx.append((i,j))
-                total += field[i][j]
-                cnt += 1
-
-
-    new_arr = deepcopy(field)
-
-    while union_idx:
-        i,j = union_idx.pop()
-        new_arr[i][j] = total//cnt
-
-    return new_arr
-
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+                if L <= abs(field[nx][ny] - field[x][y]) <= R:
+                    union.append((nx,ny))
+                    visited[nx][ny] = True
+                    dq.append((nx, ny))
+    return union
 
 ret = 0
-qqq = 0
-while qqq < 3:
-    visited = [[False for _ in range(N)] for _ in range(N)]
-    allow = [[0 for _ in range(N)] for _ in range(N)]
 
-    f = check(field,0,0)
-    for n in field:
-        print(n)
+while True:
+    flag = False
+    visited = [[0 for _ in range(N)] for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            if not visited[i][j]:
+                visited[i][j] = True
+                ul = bfs(i,j)
+                if len(ul) > 1:
+                    flag = True
+                    people = 0
+                    for p in ul:
+                        people += field[p[0]][p[1]]
+                    people = people // len(ul)
+                    for p in ul:
+                        field[p[0]][p[1]] = people
 
-    print("now arr is ",qqq)
-    for a in allow:
-        print(a)
-    if f:
-        field = move(field)
-        ret += 1
-    else:
+    if flag == False:
         break
+    ret += 1
 
-    print("new is")
 
-    qqq += 1
-print("total",ret)
-
+print(ret)
 
