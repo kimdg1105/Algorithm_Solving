be56be60939817fe2a948e9c8d20abf6f9b56def
donggyu-kim <kimss7334@naver.com>
2021-06-06 20:57:46 +0900

    21.06.06

--- BOJ/16235.py
+++ BOJ/16235.py
@@ -1,19 +1,22 @@
+import sys
 from collections import deque
 
-N, M, K = map(int,input().split())
+input = sys.stdin.readline
+
+N, M, K = map(int, input().split())
 
 field = [[5 for _ in range(N)] for _ in range(N)]
 
 s2d2 = []
+
 for _ in range(N):
-    s2d2.append(list(map(int,input().split())))
+    s2d2.append(list(map(int, input().split())))
 
-new_tree = []
-dead = []
+tree = [[[] for _ in range(N)] for _ in range(N)]
+
 for _ in range(M):
-    x,y,z = map(int,input().split())
-    new_tree.append([x,y,z])
-
+    x,y,z = map(int, input().split())
+    tree[x-1][y-1].append(z)
 
 def log():
     print("now field:")
@@ -22,36 +25,49 @@ def log():
 
     print('---')
     print("now trees")
-    for n in new_tree:
+    for n in tree:
         print(n)
 
 
 def spring():
-    new_tree.sort(key=lambda x: x[2])
-    for i in range(len(new_tree)):
-        if field[new_tree[i][0]-1][new_tree[i][1]-1] - new_tree[i][2] < 0:
-            dead.append(new_tree[i])
-            new_tree.pop(i)
-        else:
-            field[new_tree[i][0]-1][new_tree[i][1]-1] -= new_tree[i][2]
-            new_tree[i][2] += 1
+    dead = [[[] for _ in range(N)] for _ in range(N)]
 
-def summer():
-    for i in range(len(dead)):
-        field[dead[i][0]-1][dead[i][1]-1] += int(dead[i][2] / 2)
-        dead.pop(i)
+    for i in range(N):
+        for j in range(N):
+            if tree[i][j]:
+                tree[i][j].sort()
+                for k in range(len(tree[i][j])):
+                    if tree[i][j][k] != -1:
+                        if field[i][j] - tree[i][j][k] < 0:
+                            dead[i][j].append(k)
+                        else:
+                            field[i][j] -= tree[i][j][k]
+                            tree[i][j][k] += 1
+                for idx in range(len(dead[i][j])-1,-1,-1):
+                    temp = tree[i][j][dead[i][j][idx]]
+                    del tree[i][j][dead[i][j][idx]]
+                    field[i][j] += int(temp / 2)
+
+    # print("Spring",tree)
+
 
 def fall():
-    global new_tree
-    move = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-    for tree in new_tree:
-        if tree[2] % 5 == 0:
-            for m in move:
-                nx = tree[0] + m[0]
-                ny = tree[1] + m[1]
+    temp = []
+    move = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
 
-                if 0 <= nx < N and 0 <= ny < N:
-                    new_tree.append([nx,ny,1])
+    for i in range(N):
+        for j in range(N):
+            if tree[i][j]:
+                for k in range(len(tree[i][j])):
+                    if tree[i][j][k] % 5 == 0:
+                        for m in move:
+                            nx = i + m[0]
+                            ny = j + m[1]
+                            if 0 <= nx < N and 0 <= ny < N:
+                                tree[nx][ny].append(1)
+
+    # print("Fall",tree)
+
 
 def winter():
     for i in range(N):
@@ -59,10 +75,17 @@ def winter():
             field[i][j] += s2d2[i][j]
 
 
+
 for _ in range(K):
+    # log()
     spring()
-    summer()
     fall()
     winter()
 
-print(len(new_tree))
+ret = 0
+for i in range(N):
+    for j in range(N):
+        ret += len(tree[i][j])
+
+
+print(ret)
