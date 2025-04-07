93fe20f3c122638cfc0d3802b95775186e79f8d4
kimdg1105 <kimss7334@naver.com>
2025-04-07 14:22:51 +0900

    add: 25.04.04

--- programmers/42579.py
+++ programmers/42579.py
@@ -1,19 +1,19 @@
 def solution(genres, plays):
     answer = []
     dict = {}
+    play_count = {}
 
     idx = 0
     for genre, time in zip(genres, plays):
         if genre not in dict:
             dict[genre] = []
+            play_count[genre] = 0
         dict[genre].append((idx, time))
+        play_count[genre] += time
         idx += 1
 
-    phase = 0
-    while phase != 2:
-        if not dict:
-            break
-        top_genre = max(dict)
+    while dict:
+        top_genre = max(play_count, key=play_count.get)
 
         dict[top_genre].sort(key=lambda x: -x[1])
 
@@ -23,12 +23,13 @@ def solution(genres, plays):
                 break
             answer.append(dict[top_genre].pop(0)[0])
             cnt += 1
-        phase += 1
+
         dict.pop(top_genre)
+        play_count.pop(top_genre)
 
     return answer
 
 
 print(
     solution(["classic", "classic", "classic", "pop", "pop"], [600, 600, 150, 600, 700])
-)  # 	[4, 1, 3, 0]
+)
